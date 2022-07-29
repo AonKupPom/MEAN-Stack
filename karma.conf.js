4d58e0b50b457cd621effb778e52bab86eca364d
Aon <FinalDipThank@gmail.com>
2022-07-29 20:56:21 +0700

    push without edit api path

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/MEAN-Stack-Frontend'),
+      dir: require('path').join(__dirname, './coverage/MEAN-Stack'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'MEAN-Stack-Frontend'`, () => {
+  it(`should have as title 'MEAN-Stack'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('MEAN-Stack-Frontend');
+    expect(app.title).toEqual('MEAN-Stack');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('MEAN-Stack-Frontend app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('MEAN-Stack app is running!');
   });
 });
